ad41fa70706a817026a7515529eac797c77cea8e
XXXRef <h1deandseek@yandex.com>
2019-12-31 22:07:24 +0300

    1. Renamed CModulesContainer -> CModulesManager 2. Reworked CModulesManager

---
+++ trunkore/src/modulesmanager.cpp
@@ -0,0 +1,40 @@
+#include "modulesmanager.hpp"
+
+//====================================================================================================
+CModulesManager::~CModulesManager() inline {}
+
+//====================================================================================================
+std::shared_ptr<IModuleContainer> CModulesManager::operator[](const TYPE_JOBMODID &modID) const {
+	decltype(this->modules)::const_iterator libIter = this->modules.find(modID);
+	if (libIter == this->modules.cend()) throw ExModulesManager{ "No modules found" };
+	return libIter->second.first;
+}
+
+//====================================================================================================
+void CModulesManager::loadModule(const TYPE_JOBMODID &modID) {
+	auto libIter = this->modules.find(modID);
+	if (libIter == this->modules.end()) {
+		auto insertIter=this->modules.insert({ modID,{ std::make_shared<config::TYPE_MODULEMANAGER>(),1 } }); //insert can get rvalue refs: created pair moved
+		this->modules[modID].first->loadModule(modID);
+	}
+	else ++(libIter->second.second);
+}
+
+//====================================================================================================
+void CModulesManager::unloadModule(const TYPE_JOBMODID &modID) {
+	auto libIter = this->modules.find(modID);
+	if (libIter == this->modules.end()) throw ExModulesManager{ "No modules found" };
+	if ((--(libIter->second.second)) == 0) {
+		libIter->second.first->unloadModule();
+		this->modules.erase(libIter);
+	}
+}
+
+//CModulesManager::ExModulesContainer
+//====================================================================================================
+CModulesManager::ExModulesManager::ExModulesManager(const std::string &par_exInfo) inline: exInfo(par_exInfo) {}
+
+//====================================================================================================
+std::string CModulesManager::ExModulesManager::getInfo() const inline {
+	return std::string("[CModulesManager EXCEPTION] ") + exInfo;
+}
