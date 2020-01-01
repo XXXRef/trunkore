7d474750f2d04810fdd8779740141920737fcf07
XXXRef <h1deandseek@yandex.com>
2020-01-01 22:55:41 +0300

    1. Renamed ModuleManager->ModuleContainer 2. Updated CWinModuleContainer

--- trunkore/src/modulesmanager.cpp
+++ trunkore/src/modulesmanager.cpp
@@ -14,7 +14,7 @@ std::shared_ptr<IModuleContainer> CModulesManager::operator[](const TYPE_JOBMODI
 void CModulesManager::loadModule(const TYPE_JOBMODID &modID) {
 	auto libIter = this->modules.find(modID);
 	if (libIter == this->modules.end()) {
-		auto insertIter=this->modules.insert({ modID,{ std::make_shared<config::TYPE_MODULEMANAGER>(),1 } }); //insert can get rvalue refs: created pair moved
+		auto insertIter=this->modules.insert({ modID,{ std::make_shared<config::TYPE_MODULECONTAINER>(),1 } }); //insert can get rvalue refs: created pair moved //TIP Specific module container object can be created through factory mechanism and then passed to Modules Manager
 		this->modules[modID].first->loadModule(modID);
 	}
 	else ++(libIter->second.second);
